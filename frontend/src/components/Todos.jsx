188fce6b13d0559f7349f776dde8c3870e733447
Yash <yg232004@gmail.com>
2024-03-26 04:19:26 +0530

    added update and delete functionality

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 const Todos = ({ todos }) => {
+  const handleUpdate = async (id) => {
+    const response = await fetch("http://localhost:3000/completed", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id }),
+    });
+  };
+  const handleDelete = async (id) => {
+    const response = await fetch("http://localhost:3000/delete", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id }),
+    });
+  };
   return (
     <div>
       {todos.map((todo) => {
@@ -8,8 +26,19 @@ const Todos = ({ todos }) => {
           <div key={todo.title}>
             <h1>{todo.title}</h1>
             <h2>{todo.description}</h2>
-            <button>
+            <button
+              onClick={() => {
+                handleUpdate(todo._id);
+              }}
+            >
               {todo.completed == true ? "Completed" : "Mark as Completed"}
+            </button>
+            <button
+              onClick={() => {
+                handleDelete(todo._id);
+              }}
+            >
+              Delete the task
             </button>
           </div>
         );
