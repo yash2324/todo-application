add94a749c6386749c004424b6f3f884ae91b2e8
Yash <yg232004@gmail.com>
2024-03-26 02:01:16 +0530

    backend completed

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.connect(process.env.MONGO_URL);
+
+const todoSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  completed: Boolean,
+});
+
+const todo = mongoose.model("todos", todoSchema);
+module.exports = {
+  todo,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,39 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const { todo } = require("./db");
 app.listen(3000, () => {
   console.log("App started at PORT 3000");
 });
-require("dotenv").config();
 const { createTodo, updateTodo } = require("./types");
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find({});
+  res.json({
+    todos: todos,
+  });
 });
 
-app.post("/todos", (req, res) => {});
-app.post("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
   const createData = req.body;
-  const parsedData = createData.safeParse(createData);
+  const parsedData = createTodo.safeParse(createData);
   if (!parsedData.success) {
     res.status(411).json({
       message: "You sent the wrong input data",
     });
     return;
   }
+  await todo.create({
+    title: createData.title,
+    description: createData.description,
+    completed: false,
+  });
+  res.json({
+    message: "todo created",
+  });
 });
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const updatedData = req.body;
   const parsedData = updateTodo.safeParse(updatedData);
   if (!parsedData.success) {
@@ -32,4 +42,15 @@ app.put("/completed", (req, res) => {
     });
     return;
   }
+  await todo.updateOne(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+  res.json({
+    message: "marked as completed",
+  });
 });
